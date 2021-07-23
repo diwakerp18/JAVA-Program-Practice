87b55f581de1bbed0c0149866c277b84820ec2f2
Diwakar Pathak <diwakarpathak@ninjacart.com>
2021-07-23 10:31:32 +0530

    Changes to be committed:

---
+++ src/com/company/EvenOdd.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class EvenOdd {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter Any Number :");
+        int num = scan.nextInt();
+        if (num%2 == 0 ){
+            System.out.println("Number is Even");
+        }
+        else {
+            System.out.println("Number is Odd");
+        }
+    }
+}
